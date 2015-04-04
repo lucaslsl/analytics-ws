8995b069276ad407268ef104a90a18f201ea4cd8
lucaslsl <lucas.sousa.lima@hotmail.com>
2015-04-03 22:13:37 -0400

    Send function working

--- index.js
+++ index.js
@@ -3,26 +3,12 @@
 module.exports = {
 
 
-	// connect: function(host, callback){
-	// 	// Connects to a websocket server
-	// 	var socket = require('socket.io-client')(host);
-	// }
-
-	// track: function(event, callback){
-
-	// 	// Receives an object/event and then pushes it to an array		
-
-	// }
-
-	// send: function(callback){
-
-	// }
-
 	send: function(data, host, callback){
 		var socket = require('socket.io-client')(host);
-		socket.on('connect', function(){
-			socket.emit('data', JSON.stringify(data));
-		});
+		
+		if(socket.connected){
+			socket.emit('data', JSON.stringify(data));			
+		}
 
 		if (typeof callback === "function") {
 			callback(data);		
