3939f071689f069de10e2897c6a862c80890e0aa
lucaslsl <lucas.sousa.lima@hotmail.com>
2015-04-04 01:45:49 -0400

    Prototyping the module

--- index.js
+++ index.js
@@ -1,20 +1,2 @@
 
-
-module.exports = {
-
-
-	send: function(data, host, callback){
-		var socket = require('socket.io-client')(host);
-		
-		if(socket.connected){
-			socket.emit('data', JSON.stringify(data));			
-		}
-
-		if (typeof callback === "function") {
-			callback(data);		
-		}
-
-
-	}
-
-}
+module.exports = require('./lib/index.js');
